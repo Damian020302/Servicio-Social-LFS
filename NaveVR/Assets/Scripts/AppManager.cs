74fbdd66b2b2224e246a596a4488f0acd84b6580
Damian020302 <fritus32@gmail.com>
2026-06-05 11:12:30 -0600

    Se agrega una forma de configurar el brazo en el que se enfocara la terapia

--- NaveVR/Assets/Scripts/AppManager.cs
+++ NaveVR/Assets/Scripts/AppManager.cs
@@ -4,6 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class AppManager : MonoBehaviour
 {
+    [Header("UI Elements")]
+    public GameObject handMenu;
+    public GameObject mainMenu;
+
     public void Game1()
     {
         SceneManager.LoadScene("Menu");
@@ -26,5 +30,33 @@ public class AppManager : MonoBehaviour
     {
         SceneManager.LoadScene("Menu4");
         Time.timeScale = 1.0f; // Asegura que el tiempo se reanude al volver al menú
+    }
+
+    public void LeftHandSelection()
+    {
+        handMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        PlayerPrefs.SetInt("SelectedHand", 0); // 0 para mano izquierda
+        PlayerPrefs.Save();
+        HandConfigurator configurator = Object.FindFirstObjectByType<HandConfigurator>();
+        if (configurator != null)
+        {
+            configurator.ApplyConfig(0); // Aplica la configuración para mano izquierda
+        }
+        Debug.Log("Mano izquierda seleccionada");
+    }
+
+    public void RightHandSelection()
+    {
+        handMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        PlayerPrefs.SetInt("SelectedHand", 1); // 1 para mano derecha
+        PlayerPrefs.Save();
+        HandConfigurator configurator = Object.FindFirstObjectByType<HandConfigurator>();
+        if (configurator != null)
+        {
+            configurator.ApplyConfig(1); // Aplica la configuración para mano derecha
+        }
+        Debug.Log("Mano derecha seleccionada");
     }
 }
---
+++ NaveVR/Assets/Scripts/HandConfigurator.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class HandConfigurator : MonoBehaviour
+{
+    [Header("Left Hand References")]
+    public GameObject leftHandInteraction;
+    public GameObject leftHandVisuals;
+    public GameObject leftHandAnchor;
+
+    [Header("Right Hand References")]
+    public GameObject rightHandInteraction;
+    public GameObject rightHandVisuals;
+    public GameObject rightHandAnchor;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int selectedHand = PlayerPrefs.GetInt("SelectedHand", 0); // 0 para mano izquierda, 1 para mano derecha
+        ApplyConfig(selectedHand);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ApplyConfig(int selectedHand)
+    {
+        if (selectedHand == 0)
+        {
+            // Configurar para mano izquierda
+            if (leftHandInteraction != null) leftHandInteraction.SetActive(true);
+            if (leftHandVisuals != null) leftHandVisuals.SetActive(true);
+            if (leftHandAnchor != null) leftHandAnchor.SetActive(true);
+            if (rightHandInteraction != null) rightHandInteraction.SetActive(false);
+            if (rightHandVisuals != null) rightHandVisuals.SetActive(false);
+            if (rightHandAnchor != null) rightHandAnchor.SetActive(false);
+        }
+        else
+        {
+            // Configurar para mano derecha
+            if (leftHandInteraction != null) leftHandInteraction.SetActive(false);
+            if (leftHandVisuals != null) leftHandVisuals.SetActive(false);
+            if (leftHandAnchor != null) leftHandAnchor.SetActive(false);
+            if (rightHandInteraction != null) rightHandInteraction.SetActive(true);
+            if (rightHandVisuals != null) rightHandVisuals.SetActive(true);
+            if (rightHandAnchor != null) rightHandAnchor.SetActive(true);
+        }
+    }
+}
